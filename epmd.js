6bd018e68d187678eea1620069e608b66ad9bc3b
Michael Bridgen <mikeb@squaremobius.net>
2010-07-25 21:44:27 +0100

    Initial commit.  Does a node registration that epmd accepts.

---
+++ epmd.js
@@ -0,0 +1,80 @@
+var net = require('net');
+var buffer = require('buffer');
+
+var nodename = 'nodejs';
+
+// The max of any message we'll send is that for
+// ALIVE2_REQ:
+// 1  	2  	1  	1  	2  	2  	2  	Nlen  	2  	Elen
+// 120 	PortNo 	NodeType 	Protocol 	HighestVersion 	LowestVersion 	Nlen 	NodeName 	Elen 	Extra
+// = 2 + 11 + nodename.length + 2 (assuming no extra)
+var sendbuf = new buffer.Buffer(15 + nodename.length);
+
+// TODO we'll allocate recv buffers; better would be to pool them but
+// what the heck.
+
+var conn = net.createConnection(4369);
+
+function writeInt(buf, num, offset, size) {
+    // always big-endian
+    for (var i = offset + size - 1; i >= offset; i--) {
+        buf[i] = num & 0xFF;
+        num >>= 8;
+    }
+}
+
+// Write the length of the req in the first two bytes
+function writeLength(buf, length) {
+    writeInt(buf, length, 0, 2);
+}
+
+function writeString(buf, str, offset) {
+    buf.write(str, offset, 'ascii');
+}
+
+var HIDDEN = 72;
+var PROTOCOL = 0;
+var HIGHEST_VERSION = 5, LOWEST_VERSION = 5;
+
+var port = 5555;
+
+function debug_req(buf, start, end) {
+    var str = '';
+    for (var i=start; i < end; i++) {
+        str += buf[i].toString();
+        str += ','
+    }
+    console.log('Sending ' + str);
+}
+
+function send_req(buf, start, end) {
+    debug_req(buf, start, end);
+    var slice = buf.slice(start, end);
+    conn.write(slice);
+}
+
+var ALIVE2_REQ = 120;
+
+function send_alive2_req() {
+    var length = 13 + nodename.length;
+    writeLength(sendbuf, length);
+    var offset = 2;
+    sendbuf[offset++] = ALIVE2_REQ;
+    writeInt(sendbuf, port, offset, 2); offset +=2;
+    sendbuf[offset++] = HIDDEN;
+    sendbuf[offset++] = PROTOCOL;
+    writeInt(sendbuf, HIGHEST_VERSION, offset, 2); offset += 2;
+    writeInt(sendbuf, LOWEST_VERSION, offset, 2); offset += 2;
+    writeInt(sendbuf, nodename.length, offset, 2); offset += 2;
+    writeString(sendbuf, nodename, offset); offset += nodename.length;
+    // no extra.  what would it be?
+    writeInt(sendbuf, 0, offset, 2); offset +=2 ;
+    send_req(sendbuf, 0, offset);
+}
+
+conn.on('connect', function() {
+    send_alive2_req(conn);
+    conn.on('data', function(data) {
+        console.log('Recieved ' + data);
+    });
+});
